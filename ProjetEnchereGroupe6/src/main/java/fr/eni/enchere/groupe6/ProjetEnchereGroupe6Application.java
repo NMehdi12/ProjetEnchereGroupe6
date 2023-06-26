60cd49e18742945cae05127bd099a0149678eff3
mneggaoui2023 <mneggaoui2023@12SE22-4RLRZS2.ad.campus-eni.fr>
2023-06-26 10:39:59 +0200

    Initialisation du projet

---
+++ ProjetEnchereGroupe6/src/main/java/fr/eni/enchere/groupe6/ProjetEnchereGroupe6Application.java
@@ -0,0 +1,13 @@
+package fr.eni.enchere.groupe6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetEnchereGroupe6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetEnchereGroupe6Application.class, args);
+	}
+
+}
