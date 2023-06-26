7273d77dacad30555a403a2d4aea5b88edeb6d07
mneggaoui2023 <mneggaoui2023@12SE22-4RLRZS2.ad.campus-eni.fr>
2023-06-26 14:13:20 +0200

    it's okay

---
+++ ProjetEnchereGroupe6/src/test/java/fr/eni/enchere/groupe6/ProjetEnchereGroupe6ApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.eni.enchere.groupe6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetEnchereGroupe6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
