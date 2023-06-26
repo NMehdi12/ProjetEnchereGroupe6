56388991bacee2768bcb49d0deb95b4bed32f2c2
cmathieu2023 <cmathieu2023@12SE22-C0LRZS2.ad.campus-eni.fr>
2023-06-26 15:56:39 +0200

    Ajout EnchereController

---
+++ ProjetEnchereGroupe6/src/main/java/fr/eni/enchere/groupe6/controllers/EnchereController.java
@@ -0,0 +1,80 @@
+package fr.eni.enchere.groupe6.controllers;
+
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+
+
+@Controller
+@RequestMapping
+public class EnchereController {
+	@GetMapping ({"/", "/encheres"})
+	public String afficherListeEnchere() {
+		return "PageAccueilNonConnecte";
+	}
+	
+	@GetMapping ("/connexion")
+	public String seConnecter() {
+		return "PageConnexion";
+	}
+	
+	@GetMapping ("/inscription")
+	public String creerCompte() {
+		return "PageCreerCompte";
+	}
+	
+	@PostMapping ("/inscription")
+	public String enregistrerCompte() {
+		return "redirect:/inscription";
+	}
+		
+	@GetMapping ("/encheresConnecte")
+	public String afficherListeEnchereConnecte() {
+		return "PageListeEncheresConnecte";
+	}
+	
+	@PostMapping ("/encheres")
+	public String afficherMesVentes() {
+	return "PageListeEnchereMesVentes";
+	}
+	
+	
+	@GetMapping ("/profil")
+	public String consulterProfil() {
+		return "PageProfil";
+	}
+	
+	@PostMapping ("/monProfil")
+	public String modifierProfil() {
+		return "redirect:/monProfil";
+	}
+	
+	@GetMapping ("/nouvelleVente")
+	public String vueAjouterVente() {
+		return "PageVendreUnArticle";
+	}
+	
+	@PostMapping ("/nouvelleVente")
+	public String ajouterVente() {
+		return "redirect:/encheresConnecte";
+	}
+	
+	@GetMapping ("/vente")
+	public String modifierVente() {
+		return "PageEnchereNonCommencee";
+	}
+	
+	
+	@PostMapping ("/enchere")
+	public String enregistrerVente() {
+		return "redirect:/encheres";
+	}
+	
+	
+	
+}
