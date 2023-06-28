5b171bc515fa0496b7c8ddd4fb435dfd4ecf0642
cmathieu2023 <cmathieu2023@12SE22-C0LRZS2.ad.campus-eni.fr>
2023-06-28 10:00:40 +0200

    Interface ArticleVenduService et implementation

--- ProjetEnchereGroupe6/src/main/java/fr/eni/enchere/groupe6/bll/ArticleVenduServiceImpl.java
+++ ProjetEnchereGroupe6/src/main/java/fr/eni/enchere/groupe6/bll/ArticleVenduServiceImpl.java
@@ -1,5 +1,42 @@
 package fr.eni.enchere.groupe6.bll;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import fr.eni.enchere.groupe6.bo.ArticleVendu;
+import fr.eni.enchere.groupe6.dal.ArticleVenduDAO;
+
+
 public class ArticleVenduServiceImpl implements ArticleVenduService {
+
+	private ArticleVenduDAO articleVenduDao;
+	
+	@Autowired
+	public ArticleVenduServiceImpl(ArticleVenduDAO articleVenduDao) {
+		this.articleVenduDao = articleVenduDao;
+	}
+	
+	@Override
+	public List<ArticleVendu> afficherArticlesVendus() {
+		return articleVenduDao.findAll();
+	}
+
+	@Override
+	public List<ArticleVendu> afficherArticlesVendusParEtat() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ArticleVendu afficherDetailParNoArticle(Integer noArticle) {
+		return articleVenduDao.findById(noArticle);
+	}
+
+	@Override
+	public List<ArticleVendu> afficherResultatRecherche(String nomArticle) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
