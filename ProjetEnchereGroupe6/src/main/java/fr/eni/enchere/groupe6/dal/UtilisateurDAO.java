ddc209f6db9a048e4abab89bf354d0a6850d99d5
cmathieu2023 <cmathieu2023@12SE22-C0LRZS2.ad.campus-eni.fr>
2023-06-30 15:43:48 +0200

    pageCréerCompte + modif utilisateur DAO et Service pour verif pseudo

--- ProjetEnchereGroupe6/src/main/java/fr/eni/enchere/groupe6/dal/UtilisateurDAO.java
+++ ProjetEnchereGroupe6/src/main/java/fr/eni/enchere/groupe6/dal/UtilisateurDAO.java
@@ -2,12 +2,16 @@ package fr.eni.enchere.groupe6.dal;
 
 import java.util.List;
 
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+
 import fr.eni.enchere.groupe6.bo.ArticleVendu;
 import fr.eni.enchere.groupe6.bo.Utilisateur;
+import fr.eni.enchere.groupe6.dal.UtilisateurDAOImpl.UtilisateurRowMapper;
 
 public interface UtilisateurDAO {
 	List<Utilisateur> findAll();
 	Utilisateur findById(Integer id);
 	void save(Utilisateur utilisateur);
 	void delete(Integer noUtilisateur);
+	Utilisateur findByPseudo(String pseudo);
 }
--- ProjetEnchereGroupe6/src/main/java/fr/eni/enchere/groupe6/dal/UtilisateurDAOImpl.java
+++ ProjetEnchereGroupe6/src/main/java/fr/eni/enchere/groupe6/dal/UtilisateurDAOImpl.java
@@ -18,6 +18,7 @@ public class UtilisateurDAOImpl implements UtilisateurDAO {
 
 	private final static String SELECT_ALL = "select no_utilisateur, pseudo, nom, prenom, email, telephone, rue, code_postal, ville, mot_de_passe, credit, administrateur from UTILISATEURS";
 	private static final String FIND_BY_NO_USER = "select * from UTILISATEURS where no_utilisateur=:no_utilisateur";
+	private static final String FIND_BY_PSEUDO = "select * from UTILISATEURS where pseudo=:pseudo";
 	private static final String INSERT = "insert into UTILISATEURS (pseudo, nom, prenom, email, telephone, rue, code_postal, ville, mot_de_passe, credit, administrateur) "
 			+ "values (:pseudo, :nom, :prenom, :email, :telephone, :rue, :code_postal, :ville, :mot_de_passe, :credit, :administrateur)";
 	private final static String DELETE = "delete  UTILISATEURS where no_utilisateur= :noUtilisateur";
@@ -68,8 +69,21 @@ public class UtilisateurDAOImpl implements UtilisateurDAO {
 	}
 
 	@Override
+	public Utilisateur findByPseudo(String pseudo){
+		MapSqlParameterSource paramSrc = new MapSqlParameterSource("pseudo", pseudo);
+List<Utilisateur> utilisateurs = njt.query(FIND_BY_PSEUDO, paramSrc, new UtilisateurRowMapper());
+	    
+	    if (utilisateurs.isEmpty()) {
+	        System.out.println("passe par findbyPseudo utilisateur n'existe pas");
+	        return null;
+	    } else {
+	        System.out.println("passe par findbyPseudo l'utilisateur existe");
+	        return utilisateurs.get(0);
+	    }
+	}
+		
+	@Override
 	public void save(Utilisateur utilisateur) {
-
 		MapSqlParameterSource paramSrc = new MapSqlParameterSource("pseudo", utilisateur.getPseudo());
 		paramSrc.addValue("nom", utilisateur.getNom());
 		paramSrc.addValue("prenom", utilisateur.getPrenom());
