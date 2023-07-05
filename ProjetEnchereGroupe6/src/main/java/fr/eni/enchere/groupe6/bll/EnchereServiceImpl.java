d186b73927e6d92ff94678b6dba262b2eb79c9a2
AlexCard44 <alexiacarduner@gmail.com>
2023-07-05 12:13:31 +0200

    Méthodes pour tout ce qui concerne les enchères (pas fini)

--- ProjetEnchereGroupe6/src/main/java/fr/eni/enchere/groupe6/bll/EnchereServiceImpl.java
+++ ProjetEnchereGroupe6/src/main/java/fr/eni/enchere/groupe6/bll/EnchereServiceImpl.java
@@ -1,5 +1,23 @@
 package fr.eni.enchere.groupe6.bll;
 
+import org.springframework.stereotype.Service;
+
+import fr.eni.enchere.groupe6.bo.ArticleVendu;
+import fr.eni.enchere.groupe6.bo.Enchere;
+
+@Service
 public class EnchereServiceImpl implements EnchereService {
+
+	@Override
+	public void enregistrerEnchere(ArticleVendu articleVendu, Enchere enchere) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Enchere afficherMeilleureEnchereParNoArticle(Integer noArticle) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
